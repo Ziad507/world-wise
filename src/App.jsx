308c3c7fcd98367c5f00cdda240a4a95911209ef
Ziad Yasser <106494651+Ziad507@users.noreply.github.com>
2025-01-11 20:21:46 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { CitiesProvider } from "../contexts/CitiesContext";
+import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
 import ProtectedRoute from "./pages/ProtectedRoute";
 import SpinnerFullPage from "./components/SpinnerFullPage";
